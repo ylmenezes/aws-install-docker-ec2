5b7b6ca18d37a688d7ba24bfa32c6e6e8e7cdbf2
Yan Menezes <yanlmenezes@gmail.com>
2023-01-26 00:07:08 -0300

    Comando para criar novo usuário e comentários

--- script_install_docker.sh
+++ script_install_docker.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo "*** Atualizando SO ***"
 apt-get update -y
+
+echo "*** Instalação  de Pacotes ***"
 apt-get install ca-certificates curl gnupg lsb-release -y
+
+echo "*** Instalação do Docker ***"
 curl -fsSL https://get.docker.com | bash
+
+echo "*** Criando usuário ***"
+useradd USER -m -s /bin/bash -p $(openssl passwd -6 123) -G sudo
