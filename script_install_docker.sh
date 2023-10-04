51a6e0f1d15a68abc8097f79423eb2a1238a1bf0
Yan Menezes <yanlmenezes@gmail.com>
2023-10-04 10:27:52 -0300

    Melhoria para nomear os parâmetros

--- script_install_docker.sh
+++ script_install_docker.sh
@@ -10,4 +10,21 @@ echo "*** Instalação do Docker ***"
 curl -fsSL https://get.docker.com | bash
 
 echo "*** Criando usuário ***"
-useradd $1 -m -s /bin/bash -p $(openssl passwd -6 $2) -G sudo
+# Check if the script is being run as root
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+# Check if the username and password arguments are provided
+if [ $# -ne 2 ]
+  then echo "Usage: $0 <username> <password>"
+  exit
+fi
+
+# Assign the username and password arguments to variables
+username=$1
+password=$2
+
+# Create the user with the provided username and password
+useradd $username -m -s /bin/bash -p $(openssl passwd -6 $password) -G sudo
