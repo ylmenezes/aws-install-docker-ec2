2d8adb36c7dbb07e29d3c3fa5c8bf57d24e18a23
Yan Menezes <yanlmenezes@gmail.com>
2024-07-03 14:27:07 -0300

    Install Docker

--- docker_install.sh
+++ docker_install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+# Ícone ASCII do Docker
 echo "       ##        ."
 echo "     ## ## ##       ==   "
 echo "  ## ## ## ## ## ##  ==="
-echo " /"""""""""""""""""""""""""""""\\___/ ==="
+echo " /\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\\___/ ==="
 echo "|                    |"
-echo " \ . . . . . . . . . . . . . . . . . . . . .  /"
-echo "   \___________________/"
+echo " \\ . . . . . . . . . . . . . . . . . . . . . /"
+echo "   \\___________________/"
 echo "           |       |"
 echo "        __|       |__"
 echo "       |               |"
@@ -15,15 +16,16 @@ echo "       |               |"
 # Função para exibir uma barra de progresso
 progress_bar() {
     local duration=$1
-    already_done() { for ((done=0; done<$elapsed; done++)); do printf "#"; done }
-    remaining() { for ((remain=$elapsed; remain<$duration; remain++)); do printf " "; done }
-    percentage() { printf "| %s%%" $(( (($elapsed)*100)/($duration)*100/100 )); }
-    clean_line() { printf "\r"; }
+    local interval=$((duration / 100))
+    local i=0
 
-    for (( elapsed=1; elapsed<=$duration; elapsed++ )); do
-        already_done; remaining; percentage
+    while [ $i -le 100 ]; do
+        printf "\rProgress: ["
+        for ((j=0; j<i; j++)); do printf "#"; done
+        for ((j=i; j<100; j++)); do printf " "; done
+        printf "] %d%%" $i
         sleep 0.1
-        clean_line
+        i=$((i + 1))
     done
     printf "\n"
 }
@@ -31,17 +33,23 @@ progress_bar() {
 # Atualizando o sistema operacional
 echo "*** Atualizando SO ***"
 apt-get update -y &
+PID=$!
 progress_bar 30
+wait $PID
 
 # Instalação de pacotes necessários
 echo "*** Instalação de Pacotes ***"
-apt-get install ca-certificates curl gnupg lsb-release -y && clear &
+apt-get install ca-certificates curl gnupg lsb-release -y &
+PID=$!
 progress_bar 30
+wait $PID
 
 # Instalando o Docker
 echo "*** Instalação do Docker ***"
-curl -fsSL https://get.docker.com | bash && clear &
+curl -fsSL https://get.docker.com | bash &
+PID=$!
 progress_bar 30
+wait $PID
 
 # Adicionando o usuário atual ao grupo do Docker
 echo "*** Configurando o grupo de usuários do Docker ***"
